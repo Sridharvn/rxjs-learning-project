508d0d1175e045808f43ceec29f0a0ef2609faf3
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-02-01 15:53:28 +0530

    Implemented observable creation and subscription in code.ts

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,16 @@
-import * as Rx from "rxjs";
-debugger;
-console.log(Rx);
+import { Observable } from "rxjs";
+
+var observable = Observable.create((observer: any) => {
+  observer.next("Hey Guys");
+});
+
+observable.subscribe((x: any) => {
+  addItem(x);
+});
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById("output").appendChild(node);
+}
