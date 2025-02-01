57c669d96012cfedd7035c131c66cb0d7866cc61
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-02-01 16:54:48 +0530

    Refactored observable subscription to include periodic updates and added unsubscribe logic

--- src/code.ts
+++ src/code.ts
@@ -4,20 +4,27 @@ var observable = Observable.create((observer: any) => {
   try {
     observer.next("Hey Guys");
     observer.next("How're you?");
-    observer.complete();
-    observer.next("Completed");
+    // observer.complete();
+    // observer.next("Completed");
+    setInterval(() => {
+      observer.next("I am Good");
+    }, 2000);
   } catch (error) {
     observer.error(error);
   }
 });
 
-observable.subscribe(
+var observer = observable.subscribe(
   (x: any) => {
     addItem(x);
   },
   (error: any) => addItem(error),
   () => addItem("Completed")
 );
+
+setTimeout(() => {
+  observer.unsubscribe();
+}, 6001);
 
 function addItem(val: any) {
   var node = document.createElement("li");
