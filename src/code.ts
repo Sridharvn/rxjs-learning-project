9df660062c71e418178cac996dce929ef76d30e6
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-02-08 22:20:13 +0530

    Refactored observable logic to create a new observable using setInterval and skipUntil for controlled emissions based on a subject trigger

--- src/code.ts
+++ src/code.ts
@@ -7,17 +7,25 @@ import {
   merge,
   map,
   from,
+  skipUntil,
 } from "rxjs";
 
-from([
-  { firstName: "Sridhar", lastName: "V N", age: "25" },
-  { firstName: "John", lastName: "Doe", age: "30" },
-  { firstName: "Jane", lastName: "Smith", age: "28" },
-  { firstName: "Alice", lastName: "Johnson", age: "35" },
-  { firstName: "Bob", lastName: "Brown", age: "40" },
-])
-  .pipe(map((person) => person.firstName))
-  .subscribe((x: any) => addItem(x));
+const observable1 = new Observable((observer) => {
+  let i = 1;
+  setInterval(() => {
+    observer.next(i++);
+  }, 1000);
+});
+
+const observable2 = new Subject();
+
+setTimeout(() => {
+  observable2.next("Hey!!");
+}, 3000);
+
+const newObs = observable1.pipe(skipUntil(observable2));
+
+newObs.subscribe((x: any) => addItem(x));
 
 function addItem(val: any) {
   const node = document.createElement("li");
