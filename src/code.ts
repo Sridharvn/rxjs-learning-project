e8dd071957da8dc91327504d2922c75895c08e85
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-02-08 12:47:16 +0530

    Enhanced observable subscription to include a second observer and improved logging for data and error handling

--- src/code.ts
+++ src/code.ts
@@ -3,12 +3,18 @@ import { Subject } from "rxjs";
 var subject = new Subject();
 
 subject.subscribe(
-  (data: any) => addItem("Added data" + data),
-  (err: any) => addItem("There is an error : " + err),
+  (data: any) => addItem(`Observer 1 Added data ${data}`),
+  (err: any) => addItem(`There is an error : ${err}`),
   () => addItem("Completed")
 );
-
 subject.next("Add Item");
+
+var observer2 = subject.subscribe((data: any) =>
+  addItem(`Observer 2 Added data ${data}`)
+);
+
+subject.next("Second thing sent");
+subject.next("Third thing sent");
 
 function addItem(val: any) {
   const node = document.createElement("li");
